d711934d6f786ce42a9fb479e762134c5703ee72
MuyiwaSaka <MuyiwaSaka@users.noreply.github.com>
2021-10-05 22:58:13 +0100

    Draft API schema
    Created a draft schema for the API

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -54,4 +54,60 @@ class Api extends RestController {
             }
         }
     }
+
+    /*
+        User
+            create
+            get
+            get_all users
+            validate email
+            deactivate user
+        Groups
+            create
+            get group details
+            get group members
+            get group permissions
+            add users to group
+            remove users from group
+            assign permission to group
+            remove permission from group
+        Permission
+            create permission
+            update permission name
+            check if user has permission.
+            get users list of permissions.
+            get list of all permissions
+            get user's list of group memberships
+            get all users and their permissions
+            delete permission
+        Sections.
+            Create section
+            update section details
+            get section
+            get list of sections
+            get list of topics in section
+            get count of topics in section
+            get count of posts in section
+            get list of sub sections
+            move section under another section
+            delete section
+        Topic
+            Create topic
+            update topic title
+            move topic to new section
+            get topic 
+            get topic author
+            get count of posts in topic
+            get topic nav path
+            get 3 similar topic titles
+            freeze topic
+            delete topic
+        Post
+            Create post
+            update post content
+            delete post
+            hide post
+            unhide post
+
+    */
 }
